68be1b354b429d7271974514f8af44e29757c514
Ukullayappa <ukullayappa1@gmail.com>
2026-05-03 07:58:33 +0530

    Debug: Downgrade Express to v4 and add request logger

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log(`[REQUEST] ${req.method} ${req.url}`);
+    next();
+});
+
 // Basic routes for testing
 app.get('/', (req, res) => {
     res.send('Backend is running!');
